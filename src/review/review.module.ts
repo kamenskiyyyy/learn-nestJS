2a20a9438d8dbf90db1a758fcccea825161e4c11
Kamenev Nikolay <79969997909@mail.ru>
2021-07-23 18:20:16 +0300

    created models for mongoDB

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ProductModel } from '../product/product.model';
+import { ReviewModel } from './review.model';
 
 @Module({
-  controllers: [ReviewController]
+  controllers: [ReviewController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ReviewModel,
+        schemaOptions: {
+          collection: 'Review',
+        },
+      },
+    ]),
+  ],
 })
 export class ReviewModule {}
