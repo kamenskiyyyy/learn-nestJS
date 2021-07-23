0e79d2040645a2ceca37a57db16d9a7f578e0902
Kamenev Nikolay <79969997909@mail.ru>
2021-07-23 17:40:44 +0300

    database connection

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
+
+export const getMongoConfig = async (
+  configService: ConfigService,
+): Promise<TypegooseModuleOptions> => {
+  return {
+    uri: getMongoString(configService),
+    ...getMongoOptions(),
+  };
+};
+
+const getMongoString = (configService: ConfigService) =>
+  `mongodb://${configService.get('MONGO_HOST')}:${configService.get(
+    'MONGO_POST',
+  )}/${configService.get('MONGO_AUTHDATABASE')}`;
+
+const getMongoOptions = () => ({
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
