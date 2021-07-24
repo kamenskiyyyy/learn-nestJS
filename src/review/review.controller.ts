b28d22a72f3d2e2f23f8d550cd47b738479513e2
Kamenev Nikolay <79969997909@mail.ru>
2021-07-24 19:37:40 +0300

    added validation for review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,6 +7,8 @@ import {
   HttpStatus,
   Param,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
@@ -16,13 +18,14 @@ import { REVIEW_NOT_FOUND } from './review.constants';
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
     return this.reviewService.create(dto);
   }
 
-  @Delete(":id")
-  async delete(@Param("id") id: string) {
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
     const deletedDoc = await this.reviewService.delete(id);
     if (!deletedDoc) {
       throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
