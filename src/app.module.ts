627de9f2b90ef7286f07c0862c3070ea868c0af2
Kamenev Nikolay <79969997909@mail.ru>
2021-07-26 21:52:49 +0300

    connected database pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TagModule],
+  imports: [TypeOrmModule.forRoot(), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
